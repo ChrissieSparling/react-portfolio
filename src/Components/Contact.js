ca92b8047d9dacb3966aad0ec9ae33db90c9a22d
Chrissie Sparling <gigglewrks@aol.com>
2022-03-01 21:17:05 -0800

    updates to contact js and homejs

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+function Contact() {
+    return (
+        <div className="Home container">
+            <div className="row">
+                <h3>You can reach me at:</h3>
+                <p>mailto:gigglewrks@aol.com</p>
+                <P>cell phone: 253-334-7175</P>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="Home container">
+            <div className="row">
+                <div className="col-8">
+                    <h2>Hello world!!!</h2>
+                    <p>Web development is the most important tool in my chest for the future. With everyone using the web, I feel like we as a society need to use this power to connect to others, get information to users that will better their lives. And I am so happy to be apart of the development community that will pave the way for the future. </p>
+                </div>
+                <div className="col-4">
+                    <img/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
